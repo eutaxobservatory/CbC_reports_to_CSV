dbc39346e56aeb6537db4b2391fd45b5d0057a38
fmesqg <francisco.gabriel@psemail.eu>
2023-01-23 15:36:02 +0000

    argparse instead of getopt

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 import pandas as pd
 from datetime import datetime
 import shutil
-from os.path import exists, join
+from os.path import exists
 import os
 import csv
-import sys
-import getopt
+import argparse
 
 from table_extraction import get_DataFrames
 from table_standardize import unify_CbCR_tables, standardize_dataframe
@@ -23,15 +22,16 @@ def extract_each_report(
     intermediate_files_dir,
     write_tables_to_dir,
     default_max_reports=100,
-    mode="",
-    human_bored=False,
+    force_rewrite=False,
+    quiet_mode=False,
 ):
     """May update the rules during execution (Rules object gets updated in-place).
     Extracted files will be named '<mnc_id>_<end_of_year>.csv' and be on the specified directory <write_tables_to>.
     (Above, extracted means the complete pipeline from pdf to standardized, unique CSV file per report.)
     Temporary files will be inside the respective '/extraction/intermediate_files/<mnc_id>_<end_of_year>/' folder, relative to root of the repo.
     They will be named '<mnc_id>_<end_of_year>_<table_number>.csv'."""
-    if mode == "hard" or mode == "h":
+    human_bored = quiet_mode
+    if force_rewrite:
         shutil.rmtree(write_tables_to_dir)
         os.makedirs(write_tables_to_dir)
     not_extracted = set(reports)
@@ -98,90 +98,27 @@ def extract_each_report(
     return not_extracted
 
 
-def main(argv):
-    # default values
-    mode_ = "s"
-    quiet_mode = False
-    input_pdf_dir = "./input/pdf_repository/"
-    after_intervention_dir = "input/files_after_human_intervention/"
-    intermediate_files_dir = "intermediate_files/"
-    write_tables_to_dir = "./output/individual_reports/"
-    rules_path = join("configuration", "rules.json")
-    metadata_path = join("configuration", "metadata.json")
-    write_justifications_to = ""
-    HELP_STRING = """(all paths are given relative to the root of the extraction directory):
-- -f: (force): forces the deletion and the re-extraction of all files flagged for extraction (as per the metadata file)
-- -q (quiet) : this option does not prompt the user whenever column or jurisdiction names are not standard. Instead, non-standard names will have a trailing "_tocheck" flag.
-- -i (input): specifies the directory where the PDFs to be extracted can be found. Default value: '../pdf_repository/'.
-- -o (output): specifies the path at which the CSV that aggregates data from all extractions will be written. Default value: '../data.csv'.
-- --after_human: specifies the directory at which the intermediate CSV that have been manually edited can be found (so that the software can pick it up from there). Default value: './files_after_human_intervention'.
-- --write_tables_to: specifies the directory at which the final CSVs for each (MNC,year) pair will be written. The full path for these final files will be `<write_tables_to>/<MNC>_<year>.csv`. Default value: './extracted_tables/'
-- --min_nb_CbCR_cols: specifies the minimum number of columns that must contain "CbCR terms" so that a table classifies as a "CbCR table". (motivation: it is often the case that multiple tables exist in the PDF page that contains the CbCR table and not all of them have data on CbCR)
-- --rules: specifies the path of the rules file. (See rules.json below) . Default value: './configuration/rules.json'
-- --metadata: specifies the path of the metadata file. (See metadata.json below). Default value: './configuration/metadata.json'"""
-
-    # values specified by operator overwrite defaults
-    try:
-        opts, _ = getopt.getopt(
-            argv,
-            "hfqi:o:r:m:",
-            [
-                "min_nb_CbCR_cols=",
-                "min_nb_countries=",
-                "after_human=",
-                "write_tables_to=",
-                "rules=",
-                "metadata=",
-                "intermediate_dir=",
-                "justifications=",
-                "help",
-            ],
-        )
-    except getopt.GetoptError as exception:
-        print(str(exception))
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-f":
-            mode_ = "h"
-        elif opt == "-q":
-            quiet_mode = True
-        elif opt == "-i":
-            input_pdf_dir = arg
-        elif opt == "--after_human":
-            after_intervention_dir = arg
-        elif opt in ["--write_tables_to", "o"]:
-            write_tables_to_dir = arg
-        elif opt in ["--rules", "-r"]:
-            rules_path = arg
-        elif opt in ["--metadata", "-m"]:
-            metadata_path = arg
-        elif opt in ["--intermediate_dir"]:
-            intermediate_files_dir = arg
-        elif opt in ["--justifications"]:
-            write_justifications_to = arg
-        elif opt in ["-h", "--help"]:
-            print(HELP_STRING)
-            return
+def main():
 
     init_time = datetime.now()
-    rules = Rules(rules_path)
-    for directory in [intermediate_files_dir, write_tables_to_dir]:
+    rules = Rules(args.rules)
+    for directory in [args.intermediate_files_dir, args.write_tables_to_dir]:
         os.makedirs(directory, exist_ok=True)
     not_extracted = extract_each_report(
-        get_reports_from_metadata(metadata_path),
+        get_reports_from_metadata(args.metadata),
         rules,
         default_max_reports=1000,
-        mode=mode_,
-        human_bored=quiet_mode,
-        input_pdf_directory=input_pdf_dir,
-        intervened_dir=after_intervention_dir,
-        intermediate_files_dir=intermediate_files_dir,
-        write_tables_to_dir=write_tables_to_dir,
+        force_rewrite=args.force_rewrite,
+        quiet_mode=args.quiet,
+        input_pdf_directory=args.input_pdf_dir,
+        intervened_dir=args.after_intervention_dir,
+        intermediate_files_dir=args.intermediate_files_dir,
+        write_tables_to_dir=args.write_tables_to_dir,
     )
 
-    rules.write(rules_path)
-    if write_justifications_to:
-        rules.export_justifications_to_csv(write_justifications_to)
+    rules.write(args.rules)
+    if args.write_justifications_to:
+        rules.export_justifications_to_csv(args.write_justifications_to)
 
     print(f" \nNot extracted ({len(not_extracted)}) below:")
     print("\n".join([str(f) for f in not_extracted]))
@@ -189,4 +126,16 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="A script to extract CbC data from PDFs.")
+    parser.add_argument("-f", "--force-rewrite", action="store_true", help="clears output directory before extracting, thus forcing a rewrite of all files.")
+    parser.add_argument("-q", "--quiet", action="store_true", help="do not prompt the user whenever column or jurisdiction names are not standard. Non-standard names will have a trailing '_tocheck' flag.")
+    parser.add_argument("-i", "--input_pdf_dir",default=os.path.join("inputs","pdf_repository"), help="the directory where the PDFs to be extracted can be found.")
+    parser.add_argument("-o", "--write_tables_to_dir", default=os.path.join("outputs","individual_reports"), help="the directory at which the final CSVs for each report will be written.")
+    parser.add_argument("-r", "--rules", default=os.path.join("inputs","rules.json"), help="the path of the rules file.")
+    parser.add_argument("-m", "--metadata", default=os.path.join("inputs","metadata.json"), help="the path of the metadata file.")
+    parser.add_argument("--intermediate_files_dir", default=os.path.join("intermediate_files"), help="the path of the metadata file.")
+    parser.add_argument("-j", "--write_justifications_to", default=None, help="the path at which to write the file with the justifications for the rules.")
+    parser.add_argument("--after_intervention_dir", default=os.path.join("inputs","files_after_human_intervention"), help="the path of the directory with manually edited input CSVs.")
+    args = parser.parse_args()
+
+    main()
